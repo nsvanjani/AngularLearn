f4eddc3bb3c7680daa2c970a9a1269f7ed8b3dd8
Anshu Wadhwani <anshu.wadhwani@cognizant.com>
2019-01-26 14:04:06 +0530

    ngModel

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-users',
     template: `<input name ='nameInput' [(ngModel)]="name">
-    <p>Hello {{ name }} I am the html</p>`
+    <p>Hello {{ name }} I am the html</p>
+    <p [textContent]="name_input"></p>`
 })
 export class UserComponent {
   name = 'Navin';
-
+  @Input() name_input;
   onUserInput(even) {
 
     this.name = (<HTMLInputElement>event.target).value;
